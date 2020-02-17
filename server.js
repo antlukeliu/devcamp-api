b070aada581d22d2747b4f06750c7e103b6edb2e
Anthony Liu <antmliu@gmail.com>
2020-02-17 02:50:43 -0500

    Adding middleware and connection to mongodb

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 'use strict';
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
-//Route files
-const bootcamps = require('./routes/bootcamp');
-
-//Load env vars
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
+const bootcamps = require('./routes/bootcamp');
+
 const app = express();
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} on mode on port ${PORT}`
+    `Server running in ${process.env.NODE_ENV} on mode on port ${PORT}`.yellow
+      .bold
   )
 );
+
+//Handle unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  server.close(() => process.exit(1));
+});
