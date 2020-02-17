937e617b83dbbea07c03c770cc82cd460e10966a
Anthony Liu <antmliu@gmail.com>
2020-02-17 00:50:01 -0500

    Adding controller and routes folder

---
+++ routes/bootcamp.js
@@ -0,0 +1,24 @@
+'user strict';
+const express = require('express');
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp
+} = require('../controllers/bootcamps');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(getBootcamps)
+  .post(createBootcamp);
+
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+module.exports = router;
